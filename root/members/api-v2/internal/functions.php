129beeed6777d8cf45691a1f4d5dba0f8909bad6
Vincent Incarvite <vincent.incarvite@gmail.com>
2020-05-12 12:00:38 -0400

    Removed all deprecated pages

--- root/members/api-v2/internal/functions.php
+++ root/members/api-v2/internal/functions.php
@@ -1,5 +1,4 @@
 <?php
 include('functions/miscellaneous.php');
-include('functions/paypal.php');
 include('functions/reroute.php');
 include('functions/sql.php');
---
+++ root/members/fwd/codeOfConduct.php
@@ -0,0 +1,4 @@
+<?php
+// Forward to the code of conduct document
+header("Location: https://docs.google.com/document/d/127QwrJbMow264pZSQZtGsb2qH0e4CqvXmYHMOQmI8eI/edit?usp=sharing", true, 307);
+?>
